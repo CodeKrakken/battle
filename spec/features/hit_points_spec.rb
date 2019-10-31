8cedd99cf045b709dcac03181193fddbfb5fae24
Student <student@Makerss-MacBook-Pro.local>
2019-10-31 15:56:26 +0000

    One way attack function integrated

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     sign_in_and_play
-    expect(page).to have_content 'Caxul vs. Bejlo'
+    expect(page).to have_content 'Bejlo: 60HP'
   end
 end
--- spec/features/testing_infastructure_nospec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Testing Infastructure' do
-  scenario 'Can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Testing infastructure working!'
-  end
-end
